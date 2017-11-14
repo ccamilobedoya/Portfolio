cca2638c07f832e860e371f79808245470cb520e
ccamilobedoya <ccamilo.bedoya@gmail.com>
2017-11-13 22:48:33 -0500

    Arrows and scroll movement

--- public/js/main.js
+++ public/js/main.js
@@ -2,21 +2,20 @@ var itemIndex = 1;
 var nextBx = 0;
 
 $('.arrow-rigth').click(function(){
-  if ($('.b-item').length > itemIndex) {
-    var nextLeft = -itemIndex * 100;
-    $('.b1').css('left', nextLeft + 'vw');
-    itemIndex++;
-    moveBackground(nextLeft);
-  }
+  moveRigth();
 });
 
 $('.arrow-left').click(function(){
-  if (itemIndex > 1) {
-    itemIndex--;
-    var nextLeft = -(itemIndex * 100) + 100;
-    $('.b1').css('left', nextLeft + 'vw');
-    moveBackground(nextLeft);
-  }
+  moveLeft();
+});
+
+$(window).bind('mousewheel', function(event) {
+    if (event.originalEvent.wheelDelta >= 0) {
+      moveLeft();
+    }
+    else {
+      moveRigth()
+    }
 });
 
 function moveBackground (actualPos) {
@@ -24,5 +23,23 @@ function moveBackground (actualPos) {
     nextBx = actualPos / (i*(i+0.5));
     $('.b' + i).css('background-position-x', nextBx + 'vw');
     //$('.b' + i).css('transform', 'translate3d('+ nextBx + 'vw,0,0)');
+  }
+}
+
+function moveRigth() {
+  if ($('.b-item').length > itemIndex) {
+    var nextLeft = -itemIndex * 100;
+    $('.b1').css('left', nextLeft + 'vw');
+    itemIndex++;
+    moveBackground(nextLeft);
+  }
+}
+
+function moveLeft() {
+  if (itemIndex > 1) {
+    itemIndex--;
+    var nextLeft = -(itemIndex * 100) + 100;
+    $('.b1').css('left', nextLeft + 'vw');
+    moveBackground(nextLeft);
   }
 }
